1dbf607055986b5cd2099fb29d6dedc92b074ace
vchauhan <vchauhan@gmail.com>
2019-12-20 22:11:41 -0700

    Started working in it

--- internal/handler/twilio.go
+++ internal/handler/twilio.go
@@ -9,20 +9,20 @@ import (
 	"github.com/matryer/way"
 )
 
+//<Record timeout="5"></Record>
 const (
 	welcomeMessage = `<Response>
-		<Say voice="alice">Please say your password after the beep.</Say>
-		<Record timeout="5"></Record>
-	</Response>`
+						  <Say voice="alice">Welcome to Chauhan Family. Please enter passcode</Say>
+						  <Gather finishOnKey="#" timeout="5"></Gather>
+					  </Response>`
 
 	authInprogressMsg = `<Response>
-		<Play>%s</Play>
+		<Play>Your password entered is correct : %s</Play>
 	</Response>`
 )
 
 //New Returns a new mux router
 func New() http.Handler {
-
 	api := way.NewRouter()
 	api.HandleFunc("POST", "/", twillioHandler)
 	return api
@@ -32,17 +32,21 @@ func twillioHandler(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Add("Content-Type", "application/xml")
 
-	// for k, v := range r.PostForm {
-	// 	fmt.Printf("%s, %s", k, v)
-	// }
+	if err := r.ParseForm(); err != nil {
+		panic(err)
+	}
 
-	// rec := r.FormValue("RecordingUrl")
+	for k, v := range r.PostForm {
+		fmt.Printf("%s : %s\n", k, v)
+	}
 
-	// if rec == "" {
-	// 	fmt.Fprintf(w, welcomeMessage)
-	// 	return
-	// }
-	// fmt.Fprintf(w, authInprogressMsg, rec)
+	passcode := r.FormValue("Digits")
+
+	if passcode == "" {
+		fmt.Fprintf(w, welcomeMessage)
+		return
+	}
+	fmt.Fprintf(w, authInprogressMsg, passcode)
 	b, _ := httputil.DumpRequest(r, true)
 	log.Printf("request is %s", b)
 
